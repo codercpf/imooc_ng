646ad5a521a5a345dbcfa7b16d9e681a65f36103
cpfhome <makingdifference@163.com>
2017-02-07 23:45:41 +0800

    done
    理解ng-if/ng-class/ng-submit等

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ angular.module('myApp',[])
                 alert('提交成功！');
         }
     })
+
+    .directive('compare',function(){
+        var o = {};
+        o.strict = 'AE';
+        o.scope = {
+            orgText: '=compare'
+        };
+        o.require = 'ngModel';
+        o.link = function(sco, ele, att, ctrl){
+            ctrl.$validators.compare = function(val){
+                return val == sco.orgText;
+            };
+
+            sco.$watch('orgText', function(){
+                ctrl.$validate();
+            })
+        };
+        return o;
+    });
